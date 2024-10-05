eb8bd88274e24a9c42da4812a122e7ec492c937b
Jarvis-000 <letshelpofficial2024@gmail.com>
2024-10-05 09:16:26 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,4 +3,4 @@ BOT_NAME = "jokerbhaisahab3"
 BOT_TOKEN = "7521089253:AAGHqiw4e7ewdc1qt7eonChimNf-Dr7dYXw"
 API_ID = 23981002
 API_HASH = "9549825af7363be26b4b53e5bb3e737a"
-MONGO_URI = "jarvis2024cluster0.5hglp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://jarvis4064:jarvis4064@cluster0.5hglp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
